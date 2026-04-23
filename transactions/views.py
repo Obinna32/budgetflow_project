c82b2637d400c62600c585b465c0cbb4789ab10a
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-04-23 23:43:52 +0100

    feat: add transaction list view with template inheritance and basic CSS

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Transaction
 
 # Create your views here.
+def transaction_list(request):
+    #This will fetch all transactions from the database
+    transactions = Transaction.objects.all().order_by('-date')
+    return render(request, 'transactions/index.html', {
+        'transactions': transactions
+    })
