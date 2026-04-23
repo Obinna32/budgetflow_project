b6499c3572078e394101f9ccdb5b22feaff7963b
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-04-23 22:28:19 +0100

    feat: add transactions app and Transaction model

---
+++ transactions/models.py
@@ -0,0 +1,17 @@
+from django.db import models, migrations
+
+# Create your models here.
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('expense', 'Expense'),
+        ('income', 'Income'),
+    ]
+    title = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.amount}"
