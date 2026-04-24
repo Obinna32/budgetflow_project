8017a976b3ca59d6ed2dae7da05480f0c9c07dac
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-04-24 23:28:43 +0100

    feat: add dashboard summary logic and summary cards UI

--- transactions/views.py
+++ transactions/views.py
@@ -2,16 +2,32 @@ from django.shortcuts import render, redirect
 from .models import Transaction
 from .forms import TransactionForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 
 # Create your views here.
 
 @login_required
 def transaction_list(request):
     #This will fetch all transactions from the database
-    transactions = Transaction.objects.all().order_by('-date')
-    return render(request, 'transactions/index.html', {
-        'transactions': transactions
-    })
+    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
+
+    #Calculate Total income
+    total_income = transactions.filter(transaction_type='income').aggregate(Sum('amount'))['amount__sum'] or 0
+
+    #Calculate Total Expense
+    total_expense = transactions.filter(transaction_type='expense').aggregate(Sum('amount'))['amount__sum'] or 0
+
+    #Calculate Net Balance
+    balance = total_income - total_expense
+
+    context = {
+        'transactions' : transactions,
+        'total_income' : total_income,
+        'total_expense': total_expense,
+        'balance': balance,
+    }
+
+    return render(request, 'transactions/index.html', context)
 
 @login_required
 def transaction_create(request):
